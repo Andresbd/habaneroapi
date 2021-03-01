22201ec8c6a4da8bf25e30dedb855a68fc8b0f68
Andres Bustamante <andres.bustamante@mentive.com.mx>
2021-02-28 22:47:29 -0600

    added mongo cluster

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017/habanero';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-router.get('/login', (req, res) => {
-
-})
 
 module.exports = router;
--- routes/workers.js
+++ routes/workers.js
@@ -2,18 +2,21 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 
+const { mongoDbPw, mongoDbDB } = require('../credentials');
+
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = `mongodb+srv://habaneroadmin:${mongoDbPw}@realmcluster.2jxcr.mongodb.net/${mongoDbDB}?retryWrites=true&w=majority`;
 
 router.get('/getWorkers', (req, res) => {
-    MongoClient.connect(url, (err, client) => {
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
         if (err) throw err
-        const db = client.db('habanero');
-        db.collection('roles').find().toArray((err, results) => {
+        const collection = client.db(mongoDbDB).collection("roles");
+        collection.find().toArray((err, results) => {
             if (err) throw err
-            res.status(200).send(results)
-        })
-        client.close();
+            res.status(200).send(results);
+            client.close();
+        });
     });
 })
 
